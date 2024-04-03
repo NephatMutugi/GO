bf76fe509f7f5d9cf80b34319f21fc2882de05b1
Nephat Muchiri <muchirinephat5@gmail.com>
2024-04-04 01:03:19 +0300

    Functions

--- cards/main.go
+++ cards/main.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-var deckSize int
-
 func main() {
-	card := "Ace of Spades" // Only use the (:=) syntax when creating a new variable
-	card = "Five of Diamonds"
-	deckSize = 50
+	card := newCard()
 	fmt.Println(card)
-	fmt.Printf("Deck Size: %d\n", deckSize)
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
